da124ce14e9720d821089106579969fb8ffb4640
Risse-9 <nenemarionel1@gmail.com>
2025-01-19 10:02:39 +0100

    Initial commit

---
+++ Scripts/blast_automate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Script to perform BLAST+ workflow
+
+# Creat BLAST database
+makeblastdb -in nucleotide_fasta_protein_homolog_model.fasta -dbtype nucl -out CARD_DB
+
+# BLAST
+for genome in GCA*.fna; do
+  output="${genome%.fna}_blast_results.txt"
+  blastn -query "$genome" -db CARD_DB -out "$output" -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore" -evalue 1e-30
+done
+
+# Concatenate all blast output files into a single file
+cat *_blast_results.txt > combined_amr_gene_hits.txt
+
+echo "All BLAST outputs have also been combined into 'combined_amr_gene_hits.txt'."
